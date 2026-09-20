9d8c63b68af3d9fdfc3247e6f664f9960c966045
Khubi Sahu <vkhubisahu14@gmail.com>
2026-09-20 13:36:03 +0530

    configured frontend link with backend

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","voyage-inky-gamma.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
